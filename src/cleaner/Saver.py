f1d2d386aa4cf62f054c27cc8f777a71a0fa473f
jcmeunier77 <jc.meunier77@gmail.com>
2020-09-05 07:46:00 +0200

    pickel 1

--- src/cleaner/Saver.py
+++ src/cleaner/Saver.py
@@ -1,5 +1,7 @@
 
 from threading import Thread
+import pickel
+import pandas as pd
 
 
 class Saver(Thread):
@@ -11,3 +13,9 @@ class Saver(Thread):
 
         super().__init__()
         self.data = data
+
+    def to_pickel (self, data):
+        for i in len (num_threading):
+            self.data.to_pickel(f"./pickel_chunk{i}.pkl")
+
+    def merge_pickel (self):
