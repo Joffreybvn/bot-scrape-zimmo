3d7478b72d984e36b89fc64dd69f1445de49cb39
noahalvarezgonzalez <noahalvarezgonzalez@gmail.com>
2020-09-06 15:25:00 +0200

    all true to 1

--- src/cleaner/Merger.py
+++ src/cleaner/Merger.py
@@ -24,7 +24,10 @@ class Merger:
 
         # TODO: Transcrire les Booléens en 0 ou 1
         # Replace None to "None" (string) in the CSV
-        self.df[["swimming pool", "garden"]] = self.df[['swimming pool', "garden"]].apply(lambda x: self.true_to_1(x))
+        for i in ["swimming pool", "open fire", "furnished", "kitchen equipment", "garden"]:
+            self.df[i] = self.df[i].apply(lambda x: self.true_to_1(x))
+
+
         self.df.fillna("None", inplace=True)
 
         self.df.to_csv("result.csv")
